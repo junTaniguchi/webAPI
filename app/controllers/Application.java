00b870a349330ce1852d2d07e1a078c84e071ce0
junTaniguchi <ultia8597@outlook.jp>
2017-07-17 22:58:16 +0900

    javaDoc precreate

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -22,15 +22,36 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
+    /**
+     * login()
+     *  ブラウザより下記URLを実行することによって、loginページをレンダリングする。
+     *      http://XXXX:9000/login
+     *  引数
+     *      なし
+     *
+     *  戻り値
+     *      login.scala.html
+    */
     public static Result login() {
       response().discardCookie("user");
     	return ok(login.render());
     }
 
+    /**
+     * postAuthenticate()
+     *  topページのモーダルウィンドウにて記入されたユーザ名、パスワードを
+     *  POST通信で受信し、データベースと照合する。
+     *
+     *  引数
+     *      なし（POST通信なので、HTTPのリクエストボディへユーザ名とパスワードが格納されている）
+     *
+     *  戻り値
+     *      boolean
+     *        True  -データベース上のレコードと一致
+     *        False -データベース上に存在しない
+    */
     @BodyParser.Of(BodyParser.Json.class)
     public static Result postAuthenticate() {
-
-      System.out.println("body.asJson()    : " + request().body().asJson());
       JsonNode json = request().body().asJson();
       System.out.println("json    : " + json);
       //Cookie未完成
@@ -40,7 +61,7 @@ public class Application extends Controller {
           String password = json.findPath("password").textValue();
           System.out.println("name     : " + name);
           System.out.println("password : " + password);
-          /*
+          /*データベース・アクセス
           ConnectDatabase cd = new ConnectDatabase();
 
           Login login = new Login();
@@ -48,6 +69,7 @@ public class Application extends Controller {
           */
           //実験用
           boolean logincheck = true;
+
           if (logincheck) {
             //Cookieを作成し、ステートレスセッションを行う
             response().setCookie("name", name);
@@ -69,11 +91,43 @@ public class Application extends Controller {
       return null;
     }
 
+    /**
+     * top()
+     *  ブラウザより下記URLを実行することによって、topページをレンダリングする。
+     *      http://XXXX:9000/top
+     *  引数
+     *      なし
+     *
+     *  戻り値
+     *      top.scala.html
+    */
     public static Result top() {
     	//return ok(top.render());
       return ok(top.render());
     }
 
+    /**
+     * getSearch()
+     *  topページにて入力されたレポート名、更新日時をキーに
+     *  一致するすべてのファイルをJSON形式で戻す。
+     *
+     *  引数
+     *      なし（GET通信なので、HTTPのURLへクエリパラメータとしてファイル名とファイルのパスが格納されている）
+     *
+     *  戻り値
+     *      JSON形式のファイル情報
+     *        {
+     *          { "file_name" : "EP100.txt"
+     *            "file_date" : "2017年12月12日 23:13.28"
+     *            "file_path" : "C:\backup\EP\20171212\EP100.txt"
+     *            "file_text" : "ファイルの中身の文字列"
+     *          },
+     *          {
+     *            上記と同様の内容
+     *          }
+     *        }
+     *
+    */
     public static Result getSearch() {
     	/* 使用方法
     	 *   メソッド： GETメソッド
@@ -81,18 +135,6 @@ public class Application extends Controller {
     	 *   query   ： file_name=
     	 *              file_date=
     	 * */
-       // Cookieの確認
-       /*
-       String user = request().cookies().get("user").value().toString();
-       if (user != "") {
-         //userのキーを取得
-         System.out.println(request().cookie("user").name());
-         //userの値を取得
-         System.out.println(request().cookie("user").value());
-       } else {
-         return ok(login.render());
-       }
-       */
 
     	SearchFile searchFile = new SearchFile("C:\\\\Users\\\\JunTaniguchi\\\\Desktop\\\\EP");
 
@@ -137,7 +179,7 @@ public class Application extends Controller {
       }
 
       try {
-        searchFile.convertFileMapping();
+        searchFile.convertFile2Mapping();
       } catch (IOException e) {
         e.printStackTrace();
       }
@@ -147,6 +189,28 @@ public class Application extends Controller {
       return ok(Json.toJson(searchFile.getFileMapList()));
     }
 
+
+    /**
+     * getDownload()
+     *  topページのダウンロードボタンで起動するメソッド。
+     *  渡されたファイルパスをもとにファイルを検索し、コンテンツタイプを変更し、ダウンロードを行う。
+     *
+     *  引数
+     *      なし（GET通信なので、HTTPのURLへクエリパラメータとしてファイル名とファイルのパスが格納されている）
+     *
+     *  戻り値
+     *      JSON形式のファイル情報
+     *        {
+     *          { "file_name" : "EP100.txt"
+     *            "file_date" : "2017年12月12日 23:13.28"
+     *            "file_path" : "C:\backup\EP\20171212\EP100.txt"
+     *            "file_text" : "ファイルの中身の文字列"
+     *          },
+     *          {上記と同様の内容
+     *          }
+     *        }
+     *
+    */
     public static Result getDownload() {
       /*
       // Cookieの確認
--- app/models/SearchFile.java
+++ app/models/SearchFile.java
@@ -9,6 +9,25 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * SearchFileクラス
+ *
+ *  ルートディレクトリより目的のファイルを探すためのクラス。
+ *  検索対象としてサブディレクトリも再帰的に探しに行く。
+ *
+ *  - 検索方法(インスタンス化の際にルートディレクトリのパスを設定しておくことが前提)
+ *       検索方法は下記の３つのメソッドとなる。
+ *       検索後はFileオブジェクトのArrayList形式でhitFileListへ格納される。
+ *     １，ファイル名のみ
+ *            recursionSearchFileName(String targetFileName)
+ *     ２、更新日時のみ
+ *            recursionSearchTargetDate(String targetDate)
+ *     ３、ファイル名、更新日時両方
+ *            recursionSearchDual(String targetFileName, String targetDate)
+ *
+ *　- 検索後の利用方法
+ *       hitFileListを連想配列へ変換し、getterで取得して利用する。
+*/
 
 public class SearchFile {
 
@@ -18,18 +37,62 @@ public class SearchFile {
 
 	private ArrayList<Map<String, Object>> fileMapList = new ArrayList<Map<String, Object>>();
 
+	/**
+	 * デフォルトコンストラクタ
+	*/
 	public SearchFile(){
 	}
 
+	/**
+	 * デフォルトコンストラクタ（オーバーロード
+	 *
+	 *  setter
+	 *  インスタンス化と同時に指揮数で渡したファイルパスが
+	 *  当インスタンスが検索する対象のルートディレクトリとなる。
+	*/
 	public SearchFile(String rootPath){
 		this.rootPath = rootPath;
 	}
 
+	/**
+	 * getFileMapList()
+	 *
+	 *  getterクラス。
+	 *  当クラスで作成された連想配列ArrayListを
+	 *  戻す。
+	 *
+	 *  引数
+	 *      なし
+	 *
+	 *  戻り値
+	 *      ArrayList<Map<String, Object>> this.fileMapList
+	 *
+	 *    連想配列（String : Object）のArrayList
+	 *        {
+	 *          { "file_name" : "EP100.txt"
+	 *            "file_date" : "2017年12月12日 23:13.28"
+	 *            "file_path" : "C:\backup\EP\20171212\EP100.txt"
+	 *            "file_text" : "ファイルの中身の文字列"
+	 *          },
+	 *          {上記と同様の内容
+	 *          }
+	 *        }
+	*/
 	public ArrayList<Map<String, Object>> getFileMapList() {
 		return this.fileMapList;
 	}
 
-	// ファイルをファイル名で検索
+	/**
+	 * recursionSearchFileName(String targetFileName)
+	 *  ファイル名をキーに名前が一致するファイルを検索する。
+	 *  一致するすべてのファイルをarrayListへ格納する。
+	 *
+	 *  引数
+	 *      String targetFileName
+	 *
+	 *  戻り値
+	 *      なし
+	*/
 	public void recursionSearchFileName(String targetFileName) throws IOException{
 		System.out.println("File Name Search");
 		System.out.println("targetFileName : " + targetFileName);
@@ -52,7 +115,17 @@ public class SearchFile {
 		}
 	}
 
-	// ファイルを更新日時で検索
+	/**
+	 * recursionSearchTargetDate(String targetDate)
+	 *  更新日時をキーに日付が一致するファイルを検索する。
+	 *  一致するすべてのファイルをarrayListへ格納する。
+	 *
+	 *  引数
+	 *      String targetDate
+	 *
+	 *  戻り値
+	 *      なし
+	*/
 	public void recursionSearchTargetDate(String targetDate) throws IOException{
 		System.out.println("File Date Search");
 
@@ -67,14 +140,26 @@ public class SearchFile {
 				recursionSearchTargetDate(targetDate);
 
 			}else if (obj.isFile()){
-				//取得したオブジェクトがファイルの場合、ファイル名が一致するかを確認する。
+				//取得したオブジェクトがファイルの場合、更新日時が一致するかを確認する。
 				if (targetDate.equals(timeToString(obj, "yyyyMMdd"))){
 					this.hitFileList.add(obj);
 				}
 			}
 		}
 	}
-	// ファイルをファイル名と更新日時で検索
+
+	/**
+	 * recursionSearchDual(String targetFileName, String targetDate)
+	 *  ファイル名と更新日時をキーに両方のキーと一致するファイルを検索する。
+	 *  一致するすべてのファイルをarrayListへ格納する。
+	 *
+	 *  引数
+	 *      String targetFileName
+	 *      String targetDate
+	 *
+	 *  戻り値
+	 *      なし
+	*/
 	public void recursionSearchDual(String targetFileName, String targetDate) throws IOException{
 		System.out.println("Dual Search");
 
@@ -96,6 +181,19 @@ public class SearchFile {
 			}
 		}
 	}
+
+	/**
+	 * timeToString(File file, String dateFormat)
+	 *  FIleオブジェクトの更新日時をdataFormatとして受け取った日付の
+	 *  フォーマットへ変換し、変換後の更新日時を返す。
+	 *
+	 *  引数
+	 *      File file
+	 *      String dateFormat
+	 *
+	 *  戻り値
+	 *      String fileDate
+	*/
 	public String timeToString(File file, String dateFormat){
 		//ファイルの更新日時
 		SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
@@ -105,9 +203,31 @@ public class SearchFile {
 		return fileDate;
 	}
 
-	// ヒットしたファイルをMap化する
+	/**
+	 * convertFile2Mapping()
+	 *
+	 *  当クラスで作成されたFileオブジェクトのArrayListを
+	 *  連想配列（String : Object）へ変換する。
+	 *
+	 *    連想配列（String : Object）のArrayList
+	 *        {
+	 *          { "file_name" : "EP100.txt"
+	 *            "file_date" : "2017年12月12日 23:13.28"
+	 *            "file_path" : "C:\backup\EP\20171212\EP100.txt"
+	 *            "file_text" : "ファイルの中身の文字列"
+	 *          },
+	 *          {上記と同様の内容
+	 *          }
+	 *        }
+	 *
+	 *  引数
+	 *      なし
+	 *
+	 *  戻り値
+	 *      なし
+	*/
 	@SuppressWarnings("null")
-	public void convertFileMapping() throws IOException {
+	public void convertFile2Mapping() throws IOException {
 		for (File file : this.hitFileList) {
 			//リストへ格納するための連想配列を用意
 			Map<String, Object> fileMap = new HashMap<>();
